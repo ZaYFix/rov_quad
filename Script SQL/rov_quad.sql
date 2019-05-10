a8f1c165d63665e6a1308b036e68965504f65b7a
ZaYFix <38220718+ZaYFix@users.noreply.github.com>
2019-05-10 18:21:59 +0200

    Add files via upload

--- Script SQL/rov_quad.sql
+++ Script SQL/rov_quad.sql
@@ -8,18 +8,22 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
-
 CREATE DATABASE rov_quad;
 
 CREATE TABLE `mesures` (
-  `date` varchar(50) NOT NULL,
-  `temperature` decimal(10,1) NOT NULL,
-  `humidite` decimal(10,1) NOT NULL,
-  `chemin_image` varchar(255) NOT NULL
+  `id` int(11) NOT NULL,
+  `date` datetime NOT NULL,
+  `temperature` decimal(3,1) NOT NULL,
+  `humidite` decimal(3,1) NOT NULL,
+  `chemin_image` varchar(255) CHARACTER SET utf8 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 ALTER TABLE `mesures`
-  ADD PRIMARY KEY (`date`);
+  ADD PRIMARY KEY (`id`);
+
+
+ALTER TABLE `mesures`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
